6517fc9e9150c8a72c78cb3bbffe4e4df2f34ccd
Michael Aguirre <michaelaguirre@me.com>
2013-03-07 13:00:34 -0800

    Dev: Mobile solution complete
    Next: clean code

--- js/main.js
+++ js/main.js
@@ -125,7 +125,7 @@ barOverColor: "#CCCCCC"		// Back bar color on over
 
 $("#dropdown").on("click", function(e){ e.preventDefault(); if($(this).hasClass("open")) { $(this).removeClass("open"); $(this).children("ul").slideUp("fast"); } else { $(this).addClass("open"); $(this).children("ul").slideDown("fast"); } });
 
-
+$("#dropdown-mobile").on("click", function(e){ e.preventDefault(); if($(this).hasClass("open")) { $(this).removeClass("open"); $(this).children("ul").slideUp("fast"); } else { $(this).addClass("open"); $(this).children("ul").slideDown("fast"); } });
 
 // Scroll to function 
 
